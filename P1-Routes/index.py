079004dc2f7f2681aae530942a9ded6843e22c71
Shubhangisaini205 <shubhangisaini205@gmail.com>
2023-07-03 14:41:45 +0530

    done

---
+++ P1-Routes/index.py
@@ -0,0 +1,20 @@
+
+from flask import Flask, request
+app = Flask(__name__)
+
+@app.route('/')
+def welcome():
+    return 'Welcome to the Flask application!!'
+
+@app.route('/greet/<username>')
+def greet_user(username):
+    return f'Hello, {username}!'
+
+@app.route('/farewell/<username>')
+def farewell_user(username):
+    return f'Goodbye, {username}!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True,port=8080)
+
